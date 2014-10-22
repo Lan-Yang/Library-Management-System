a23ee061cd5e81a475261d0fd5c3e77e361f7d02
Meng Wang <mw2972@columbia.edu>
2014-10-22 10:52:09 -0400

    list all librarys

--- mwly_dbweb.php
+++ mwly_dbweb.php
@@ -1,15 +1,27 @@
-<p>Hello world!</p>
+<!DOCTYPE html>
+<html>
+<head>
+<title>DBWEB</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
 <p>
 <?php
 	ini_set('display_errors', 'On');
 	require 'mwly_conn.php';
 	# $conn returned. Don't forget oci_close($conn) !!
-	$stmt = oci_parse($conn, "select user from dual");
+	$stmt = oci_parse($conn, "select * from library");
 	oci_execute($stmt, OCI_DEFAULT);
 	while ($res = oci_fetch_row($stmt))
 	{
-		echo "User Name: ". $res[0] ;
+		echo "Library ". $res[0] . ": " . $res[1] .  "<br>";
 	}
 	oci_close($conn);
 ?>
 </p>
+</body>
+</html>
+

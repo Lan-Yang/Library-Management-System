49c22f7f4a53c25c242242c13ca4315eb1568f83
Lan Yang <ireneyanglan@gmail.com>
2014-10-30 21:02:26 -0400

    sponsor management

---
+++ mwly_patron_ad.back.php
@@ -0,0 +1,73 @@
+<?php
+function error_return($msg, $next)
+{
+	echo '<script type="text/javascript">alert("'
+			. $msg . '");
+			location.href = "'
+			. $next . '";
+			</script>';
+	exit();
+}
+
+$nexturl = "mwly_reader_man.php";
+ini_set('display_errors', 'On');
+session_start();
+require 'mwly_conn.inc';
+if (!isset($_POST['post-type'])) 
+	header("Location:$nexturl");
+	
+switch ($_POST['post-type']) {
+case "add_patron":
+	$name = trim($_POST['patron_name']);
+	$info = trim($_POST['patron_info']);
+	if (empty($name))
+		error_return("Must have a name!", $nexturl);
+	$sql = "select max(patron_id)+1
+			 from patron";
+	$stmt = oci_parse($conn, $sql);
+	oci_execute($stmt, OCI_COMMIT_ON_SUCCESS);
+	$pid = intval(oci_fetch_row($stmt)[0]);
+	$sql = "insert into patron
+		values (
+			$pid, '$name',
+			'$info')";
+	$stmt = oci_parse($conn, $sql);
+	$ret = oci_execute($stmt, OCI_COMMIT_ON_SUCCESS);
+	if (!$ret)
+		error_return("Add patron fail!", $nexturl);
+	$sql = "insert into sponsor_of
+		values (
+			 $pid, 
+			 {$_SESSION['library_id']})";
+	$stmt = oci_parse($conn, $sql);
+	$ret = oci_execute($stmt, OCI_COMMIT_ON_SUCCESS);
+	if (!$ret)
+		error_return("Add book fail!", $nexturl);	
+	else
+		error_return("Add book success", $nexturl);	
+	break;
+case "del_patron":
+	$pid = intval($_POST['patron_id']);
+	if ($pid <= 0) {
+		error_return("Invalid patron id!", $nexturl);
+	}	
+	$sql = "select * from sponsor_of s,patron p
+		where p.patron_id=$pid
+		and s.patron_id=p.patron_id
+		and s.library_id={$_SESSION['library_id']}";
+	$stmt = oci_parse($conn, $sql);
+	oci_execute($stmt, OCI_COMMIT_ON_SUCCESS);
+	if (!oci_fetch_row($stmt))
+		error_return("No such patron!", $nexturl);		
+	$sql = "delete from patron 
+		where patron_id=$pid";
+	$stmt = oci_parse($conn, $sql);
+	$ret = oci_execute($stmt, OCI_COMMIT_ON_SUCCESS);
+	if ($ret)
+		error_return("Delete reader success", $nexturl);	
+	else
+		error_return("Delete reader fail!", $nexturl);		
+break;
+}
+
+?>
---
+++ mwly_patron_man.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<?php
+ini_set('display_errors', 'On');
+session_start();
+if (!isset($_SESSION['library_id']))
+	header("Location: mwly_dbweb.php");
+// librarian info
+require 'mwly_conn.inc';
+if (!isset($_SESSION['librarian_id']))
+	header("Location:mwly_search.php");
+$linfo_k = array("id:", "name:");
+$sql = "select * from librarian 
+	where librarian_id=" . $_SESSION['librarian_id'] .
+	" and library_id=" . $_SESSION['library_id'];
+//echo $sql;
+$stmt = oci_parse($conn, $sql);
+oci_execute($stmt, OCI_DEFAULT);
+if ($res = oci_fetch_row($stmt))
+	$linfo_v = $res;
+else
+	header("Location:mwly_search.php");
+?>
+<head>
+	<title>PatronManagement</title>
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="classes.css">
+</head>
+
+<body>
+<div id = "banner">
+	<h1><?php echo $_SESSION['library_name']?></h1>
+</div>
+<div id = "navlist">
+	<h2>PERSONAL INFO</h2>
+	<table>
+	<?php 
+	for ($i=0; $i<2; $i++) {
+		echo "<tr>";
+		echo "<td>".$linfo_k[$i]."</td>";
+		echo "<td>".$linfo_v[$i]."</td>";
+		echo "</tr>";
+	}
+	?>
+	</table>
+		<a href="mwly_librarian.php">check/return book</a>
+	<br>
+		<a href="mwly_book_man.php">add/delete book</a>
+	<br>
+		<a href="mwly_reader_man.php">add/delete reader</a>
+	<br>
+		<a href="mwly_book_trace.php">trace book</a>
+	<br>
+		<a href="mwly_search.php">return</a>
+	<br>
+		<a href="mwly_login.back.php?action=logout">log out</a>
+</div>
+<div id="result">
+<?php
+if (isset($_POST['post-type'])) {
+	switch ($_POST['post-type']) {
+	case "search_patron":
+		$sql = "select p. from patron p, sponsor_of s 
+			where s.library_id={$_SESSION['library_id']}
+			and p.patron_id=s.patron_id
+			and p.".$_POST['search_by'];
+		if ($_POST['search_by']=='patron_id')
+			$sql .= "=".intval($_POST['search_val']);
+		else
+			$sql .= " like '%".trim($_POST['search_val'])."%'";
+		$stmt = oci_parse($conn, $sql);
+		oci_execute($stmt, OCI_DEFAULT);
+		$seach_flag = 1;
+		break;
+	}
+}
+?>
+<div id="displaytwo">
+	<h2>ADD PATRON</h2>
+	<form name="add_patron" action="mwly_patron_ad.back.php" method="post">
+		<table>
+			<tr>
+				<td>name:</td>
+				<td><input type="text" name="patron_name" /></td>
+			</tr>
+			<tr>
+				<td>info:</td>
+				<td><input type="text" name="patron_info" /></td>
+			</tr>
+			<tr>
+		</table>
+		<button type="submit" name="post-type" value="add_patron">
+		Submit</button>
+	</form>
+</div>
+<div id="displaytwo">
+	<h2>DELETE PATRON</h2>
+	<form name="del_patron" action="mwly_patron_ad.back.php" method="post">
+		<table>
+			<tr>
+				<td>patron id:</td>
+				<td><input type="text" name="patron_id" /></td>
+			</tr>
+		</table>
+		<button type="submit" name="post-type" value="del_patron">
+		Submit</button>
+	</form>
+</div>
+</div>
+<div id = "search">
+	<form name="search_patron" action="" method="post">
+	SEARCH READER:
+		<select name="search_by">
+  		<option value="patron_id">patron id</option>
+  		<option value="patron_name">patron name</option>
+		</select>
+		<input type="text" name="search_val" />
+		<button type="submit" name="post-type" value="search_reader">
+		Search</button>
+	</form>
+</div>
+<div id="result2">
+	<table class="gridtable">
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Info</th>
+		</tr>
+		<?php
+		if (isset($seach_flag)) {
+			while ($res=oci_fetch_row($stmt)) {
+				echo "<tr>";
+				foreach ($res as $v)
+					echo "<td>".$v."</td>";
+				echo "</tr>";
+			}
+		}
+		?>
+	</table>
+</div>
+</body>
+</html>
